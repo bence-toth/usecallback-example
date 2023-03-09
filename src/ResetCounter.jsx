3c1a1d141de1e68599d5eea58e2e886995cecda5
Bence A. Tóth <tothab@gmail.com>
2023-03-09 10:49:53 +0100

    Desctructured props

--- src/ResetCounter.jsx
+++ src/ResetCounter.jsx
@@ -1,7 +1,7 @@
-const ResetCounter = (props) => {
+const ResetCounter = ({ counter, setCounter }) => {
   const onResetCounter = () => {
-    console.log(`The counter was reset after ${props.counter}`);
-    props.setCounter(0);
+    console.log(`The counter was reset after ${counter}`);
+    setCounter(0);
   };
 
   return <button onClick={onResetCounter}>Reset counter #2</button>;
